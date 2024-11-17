598bce516cfb7beb0dd54159172dcd6da7f67a3c
souemon <kobekobekitakita@gmail.com>
2024-11-17 22:18:04 +0900

    feat: lambdaの構築

--- lib/aws-cdk-hands-on-stack.ts
+++ lib/aws-cdk-hands-on-stack.ts
@@ -1,6 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+const REPOSITORY_TOP = path.join(__dirname, "../");
+const PREFIX = "aws-cdk-ts";
 
 export class AwsCdkHandsOnStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +16,15 @@ export class AwsCdkHandsOnStack extends cdk.Stack {
       bucketName: `${PREFIX}-bucket-souemon`,
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const resizeLambda = new NodejsFunction(this, `${PREFIX}-lambda-resize`, {
+      functionName: `${PREFIX}-resize`,
+      entry: path.join(REPOSITORY_TOP, "lambdas/resize/src/index.ts"),
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
     });
   }
 }
