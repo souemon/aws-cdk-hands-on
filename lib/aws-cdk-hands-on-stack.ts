8a4993e1aa17b24d7bae8d26339455f5d56e4f90
souemon <kobekobekitakita@gmail.com>
2024-11-17 22:12:47 +0900

    feature: s3バケット作成

--- lib/aws-cdk-hands-on-stack.ts
+++ lib/aws-cdk-hands-on-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class AwsCdkHandsOnStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkHandsOnQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, `${PREFIX}-bucket`, {
+      bucketName: `${PREFIX}-bucket-souemon`,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
